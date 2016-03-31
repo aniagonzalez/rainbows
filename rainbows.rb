c3e70c2ebe72d63b7f911e6f04ea4be6e2d1ebc9
Ania Gonzalez <aniagr@gmail.com>
2016-03-30 23:20:18 -0700

    Completed the exercise using Rainbow gem but the gem would not work with a variable for the color, so I had to use conditionals.

--- rainbows.rb
+++ rainbows.rb
@@ -1,3 +1,5 @@
+require 'rainbow'
+
 red = [255,0,0]
 green = [0,255,0]
 blue = [0,0,255]
@@ -27,3 +29,26 @@ end
 
 new_color = mix_colors(red,green)
 puts "#{color_lookup[red]} #{red} + #{color_lookup[green]} #{green} = #{color_lookup[new_color]} #{new_color}"
+
+
+rainbow = [
+  {"red" => {r: 255, g: 0, b: 0}},
+  {"orange" => {r: 255, g: 127, b: 0}},
+  {"yellow" => {r: 255, g: 255, b: 0}},
+  {"green" => {r: 0, g: 255, b: 0}},
+  {"blue" => {r: 0, g: 0, b: 255}},
+  {"indigo" => {r: 75, g: 0, b: 130}},
+  {"violet" => {r: 143, g: 0, b: 255}},
+]
+
+rainbow.length.times do |i|
+  color = rainbow[i].keys[0]
+  if color == "red" then puts Rainbow(color).red
+  elsif color == "orange" then puts Rainbow(color).orange
+  elsif color == "yellow" then puts Rainbow(color).yellow
+  elsif color == "green" then puts Rainbow(color).green
+  elsif color == "blue" then puts Rainbow(color).blue
+  elsif color == "indigo" then puts Rainbow(color).indigo
+  elsif color == "violet" then puts Rainbow(color).violet
+  end
+end
